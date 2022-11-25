38ff97ae9c26f8e6445d5f941ee0bcc2a715c09b
netsgnut <284779+netsgnut@users.noreply.github.com>
2022-11-24 21:20:29 -0800

    feat: refactor tuner logic as BaseChocolateModelTuner

---
+++ src/models/base_chocolate_model_tuner.py
@@ -0,0 +1,111 @@
+import dill
+import os
+import pandas as pd
+from scipy.stats import randint
+from sklearn.model_selection import RandomizedSearchCV
+
+
+class BaseChocolateModelTuner():
+    """
+    Encapsulates all the logic needed to tune a particular family of models.
+
+    It uses `RandomizedSearchCV` in its core. Subclasses should provide their
+    own implementation for `create_pipeline` and `param_distribution` for this
+    to work.
+
+    Attributes
+    ----------
+    pipeline : sklearn.pipeline.Pipeline or None
+        The pipeline object to run the training with
+    search_n_iter : int
+        Number of iterations to run our `RandomizedSearchCV` with, defaulted
+        to 20
+    search_cv : int
+        Number of slices in CV, defaulted to 5
+    tuned_file_name : str
+        Name of the tuned model file, defaulted to `"model.joblib"`
+    """
+
+    def __init__(self):
+        self.pipeline = None
+        self.search_n_iter = 20
+        self.search_cv = 5
+        self.tuned_file_name = 'model.joblib'
+
+    def tune_and_dump(self, train_df_path, model_dump_dir):
+        """
+        Tune a model and then dump to specific directory
+
+        Parameters
+        ----------
+        train_df_path : str
+            Path to the training dataframe CSV file
+        model_dump_dir : str
+            Path to dump the model to
+        """
+        TARGET = 'rating'
+
+        # Load data and split into features and target
+        train_df = pd.read_csv(train_df_path)
+        X_train = train_df.drop(columns=["rating"])
+        y_train = train_df["rating"]
+
+        # Create the pipeline for modelling
+        self.pipeline = self.create_pipeline()
+        self.pipeline.fit(X_train, y_train)
+
+        # Tune hyperparameters
+        param_dist = self.param_distribution()
+
+        # Perform `RandomizedSearchCV`
+        random_search_cv = RandomizedSearchCV(
+            self.pipeline,
+            param_distributions=param_dist,
+            n_iter=self.search_n_iter,
+            cv=self.search_cv,
+            n_jobs=-1,
+            random_state=522
+        )
+
+        random_search_cv.fit(X_train, y_train)
+
+        # Check if the directory already exists
+        try:
+            os.makedirs(model_dump_dir)
+        except FileExistsError:
+            pass
+
+        # Save the model
+        with open(f'{model_dump_dir}/{self.tuned_file_name}', 'wb') as file:
+            dill.dump(random_search_cv, file)
+
+    def create_pipeline(self):
+        """
+        Create pipeline
+
+        Raises
+        ------
+        NotImplementedError
+        """
+        raise NotImplementedError("create_pipeline() not properly implemented")
+
+    def param_distribution(self):
+        """
+        Get param distribution
+
+        Returns
+        -------
+        dict :
+            a dictionary pair to be passed to `RandomizedSearchCV` as
+            `param_dist`
+        """
+        len_vocab = len(
+            self.pipeline.named_steps["columntransformer"]
+                .named_transformers_["countvectorizer"]
+                .get_feature_names_out()
+        )
+
+        return {
+            "columntransformer__countvectorizer__max_features":
+                randint(low=100, high=len_vocab)
+        }
--- src/models/chocolate_svm_rbf.py
+++ src/models/chocolate_svm_rbf.py
@@ -1,72 +1,60 @@
 # author: Manvir Kohli, Julie Song, Kelvin Wong
 # date: 2022-11-26
 
-"""This script creates an SVM RBF (Support Vector Machine with Radial Basis Function kernel) model using the preprocessed input features from the chocolate exploration dataset.
-It outputs a tuned SVM RBF model.
+"""This script creates an SVM RBF (Support Vector Machine with Radial Basis
+Function kernel) using the preprocessed input features from the chocolate
+exploration dataset. It dumps a tuned SVM RBF model.
 
-Usage: src/chocolate_svmrbf_model.py --train=<training_ds> --output=<output_folder>
+Usage: ./src/models/chocolate_svm_rbf.py --train=<training_ds> --output=<output_folder>
 
 Options:
---train=<training_ds>          Path to the training dataset, which is a .csv file
---output=<output_folder>       Path to the folder to save the modelling output to
-""" 
+--train=<training_ds>       Path to the training dataset, which is a .csv file
+--output=<output_folder>    Path to the folder to save the modelling output to
+"""
 
 from docopt import docopt
-from ..preprocessor.chocolate import preprocessor
-from sklearn.svm import SVR
-from sklearn.model_selection import cross_validate
+from scipy.stats import loguniform
 from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import RandomizedSearchCV
-from scipy.stats import randint, loguniform
-import pickle
-import dill
-import pandas as pd
-import os
+from sklearn.svm import SVR
+
+from ..preprocessor.chocolate import preprocessor
+from .base_chocolate_model_tuner import BaseChocolateModelTuner
+
+
+class ChocolateSvmRbfTuner(BaseChocolateModelTuner):
+    def __init__(self):
+        super().__init__()
+        self.tuned_file_name = "tuned_svm_rbf.joblib"
+
+    def create_pipeline(self):
+        """
+        Create pipeline
+
+        Returns
+        -------
+        sklearn.pipeline.Pipeline : the pipeline to run tuning on
+        """
+        return make_pipeline(preprocessor, SVR())
+
+    def param_distribution(self):
+        """
+        Get param distribution
+
+        Returns
+        -------
+        dict :
+            a dictionary pair to be passed to `RandomizedSearchCV` as
+            `param_dist`
+        """
+        # `columntransformer__countvectorizer__max_features` is inherited
+        return super().param_distribution() | {
+            "svr__C": loguniform(1e-3, 1e4)
+        }
+
 
 opt = docopt(__doc__)
 
-def main(train, output):
-    # Load data, split into features and target
-    train_df = pd.read_csv(train)
-    X_train = train_df.drop(columns=["rating"])
-    y_train = train_df["rating"]
-    
-    # Create the pipeline for modelling
-    pipe = make_pipeline(preprocessor, SVR())
-    pipe.fit(X_train, y_train)
-    
-    # Tune hyperparameters
-    len_vocab = len(
-    pipe.named_steps["columntransformer"]
-        .named_transformers_["countvectorizer"]
-        .get_feature_names_out()
-    )
-    
-    param_dist = {
-    "svr__C": loguniform(1e-3, 1e4),
-    "columntransformer__countvectorizer__max_features": randint(low=100, high=len_vocab)
-    }
-    
-    random_search_svc = RandomizedSearchCV(
-        pipe,
-        param_distributions=param_dist,
-        n_iter=20,
-        cv=5,
-        n_jobs=-1,
-        random_state=522
-    )
-    
-    random_search_svc.fit(X_train, y_train);
-    
-    # Check if the directory already exists
-    try:
-        os.makedirs(output)
-    except FileExistsError:
-        pass
-    
-    # Save the model
-    with open(f'{output}/tuned_svm_rbf.joblib', 'wb') as file:
-        dill.dump(random_search_svc, file)
-    
 if __name__ == "__main__":
-    main(opt["--train"], opt["--output"])
+    tuner = ChocolateSvmRbfTuner()
+    tuner.tune_and_dump(
+        train_df_path=opt["--train"], model_dump_dir=opt["--output"])
